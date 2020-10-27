024cca1a401b458b3bc2d5fb4830638a66513e1c
Carson Meiling <carmc93@gmail.com>
2020-10-26 19:48:02 -0600

    api call rendering

--- src/components/forecast/Forecast.js
+++ src/components/forecast/Forecast.js
@@ -4,43 +4,34 @@ import axios from 'axios';
 
 const Forecast = () => {
 
-  
-  componentDidMount() {
-    const options = {
-      method: 'GET',
-      url: 'https://rapidapi.p.rapidapi.com/weather',
-      params: {
-        q: 'London,uk',
-        lat: '0',
-        lon: '0',
-        callback: 'test',
-        id: '2172797',
-        lang: 'null',
-        units: '"metric" or "imperial"',
-        mode: 'xml, html'
-      },
-      headers: {
-        'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com',
-        'x-rapidapi-key': '0a157b1b2emsh1de672134471601p14a7e7jsn8ee1f917a58b',
-      }
-    };
-    
-    axios.request(options).then(function (response) {
-      console.log(response.data);
-    }).catch(function (error) {
-      console.error(error);
-    });
+  let [responseObj, setResponseObj] = useState({});
+
+  function getForecast() {
+    fetch("https://rapidapi.p.rapidapi.com/weather?q=London%2Cuk&lat=0&lon=0&callback=test&id=2172797&lang=null&units=%22metric%22%20or%20%22imperial%22&mode=xml%2C%20html", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
+		"x-rapidapi-key": "0a157b1b2emsh1de672134471601p14a7e7jsn8ee1f917a58b"
+	}
+})
+.then(response =>
+  response.json())
+.then(response => {
+  setResponseObj(response)
+  })
+.catch(err => {
+	console.error(err);
+});
   }
-
-  return(
-
+  return (
     <div>
-           <h2>Find Current Weather Conditions</h2>
-           <div>
-               {JSON.stringify(responseObj)}
-           </div>
-           <button onClick={getForecast}>Get Forecast</button>
-       </div>
+      <h2>Find Current Weather Conditions</h2>
+      <div>
+          {JSON.stringify(responseObj)}
+      </div>
+      <button onClick={getForecast}>Get Forecast</button>
+    </div>
   )
+  
 }
 export default Forecast;
