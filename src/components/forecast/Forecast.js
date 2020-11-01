1de3bba34f94f9aba5dcc10805bbb0118fc222a7
Carson Meiling <carmc93@gmail.com>
2020-11-01 12:25:56 -0700

    added form for location and units

--- src/components/forecast/Forecast.js
+++ src/components/forecast/Forecast.js
@@ -1,13 +1,20 @@
 import React, { useState, Component } from 'react';
 import axios from 'axios';
+import Conditions from '../conditions/Conditions'
 
 
 const Forecast = () => {
 
   let [responseObj, setResponseObj] = useState({});
+  let [city, setCity] = useState('');
+  let [unit, setUnit] = useState('imperial');
 
-  function getForecast() {
-    fetch("https://rapidapi.p.rapidapi.com/weather?q=London", {
+  const uriEncodedCity = encodeURIComponent(city);
+
+  function getForecast(e) {
+    e.preventDefault();
+    
+    fetch(`https://rapidapi.p.rapidapi.com/weather?units=${unit}&q=${uriEncodedCity}`, {
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
@@ -27,10 +34,39 @@ const Forecast = () => {
   return (
     <div>
       <h2>Find Current Weather Conditions</h2>
-      <div>
-          {JSON.stringify(responseObj)}
-      </div>
-      <button onClick={getForecast}>Get Forecast</button>
+      <form onSubmit={getForecast}>
+        <input
+          type="text"
+          placeholder="Enter City"
+          maxLength="50"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+        />
+        <label>
+          <input
+            type="radio"
+            name="units"
+            checked={unit === "imperial"}
+            value="imperial"
+            onChange={(e) => setUnit(e.target.value)}
+          />
+            Fahrenheit
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="units"
+            checked={unit === "metric"}
+            value="metric"
+            onChange={(e) => setUnit(e.target.value)}
+          />
+            Celcius
+        </label>
+        <button type="submit">Get Forecast</button>
+    </form>
+      <Conditions
+        responseObj={responseObj}
+      />
     </div>
   )
   
